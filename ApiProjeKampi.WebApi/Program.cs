44af876d4822b7d6615677b37963d7366305f9d0
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-04-21 19:24:39 +0300

    ders 24 sonu

--- ApiProjeKampi.WebApi/Program.cs
+++ ApiProjeKampi.WebApi/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<ApiContext>();
 builder.Services.AddScoped<IValidator<Product>, ProductValidator>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- ApiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ ApiProjeKampi.WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi.WebUI.Dtos.ServicesDtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi.WebUI.ViewComponents
 {
     public class _ServiceDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7190/api/Services/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServicesDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
